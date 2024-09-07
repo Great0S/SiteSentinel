0915aef282d9645fa8d5631afc04e3400b6562e3
Great0S <24699619+Great0S@users.noreply.github.com>
2024-09-07 19:14:05 +0300

    added website screenshots for better analysis

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
+import asyncio
 from flask import Flask, make_response, render_template
 import threading
-
 from weasyprint import CSS, HTML
-from website_monitor.monitor import websites, start_monitoring
+from website_monitor.monitor import periodic_monitoring, websites
 
 app = Flask(__name__)
 
-# Start the monitoring in a separate thread so it runs in the background
-monitor_thread = threading.Thread(target=start_monitoring)
-monitor_thread.daemon = True
+# Start the async monitoring in a separate thread
+monitor_thread = threading.Thread(target=periodic_monitoring, daemon=True)
 monitor_thread.start()
+websites_list = dict(sorted(websites.items(), key=lambda item: item[1]['status'], reverse=False))
 
 @app.route("/")
 def index():
-    # Sort websites by status ('Up', 'Error', 'Down')
-    sorted_websites = dict(sorted(websites.items(), key=lambda item: item[1]['status'], reverse=False))
-    return render_template("index.html", websites=sorted_websites)
+    try:        
+        return render_template("index.html", websites=websites_list)
+    except Exception as e:
+        return f"An error occurred: {e}", 500
 
 @app.route("/export_pdf")
 def export_pdf():
-    # Render the HTML template
-    html = render_template("index.html", websites=websites)
-    
-    # Create a PDF from the rendered HTML
-    css = CSS(string='@page { size: A4; margin: 0.5cm }, @button { width: 20px;}')
-    pdf = HTML(string=html).write_pdf(stylesheets=[css])
-    
-    # Create a response object
-    response = make_response(pdf)
-    response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = 'attachment; filename=website_status.pdf'
-    return response
+    try:
+        html = render_template("index.html", websites=websites_list)
+        
+        # Create a PDF from the rendered HTML
+        css = CSS(string='@page { size: A4; margin: 0.5cm; }')
+        pdf = HTML(string=html).write_pdf(stylesheets=[css])
+        
+        # Create a response object
+        response = make_response(pdf)
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = 'attachment; filename=website_status.pdf'
+        return response
+    except Exception as e:
+        return f"An error occurred: {e}", 500
 
 if __name__ == "__main__":
     app.run(debug=True)
--- website_monitor/monitor.py
+++ website_monitor/monitor.py
@@ -1,56 +1,151 @@
-import smtplib
-import socket
+import asyncio
 import pandas as pd
-import requests
+import aiohttp
+import smtplib
 import logging
-import threading
 import os
+import json
+import socket
 import time
+from playwright.sync_api import sync_playwright
+from threading import Thread
 
-# Load environment variables
+import requests
+
+# Constants
+EMAIL_SERVER = os.getenv("EMAIL_SERVER")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_SENDER = os.getenv("EMAIL_SENDER")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_RECEIVER = os.getenv("EMAIL_RECEIVER")
+ERROR_THRESHOLD = 3
+RETRY_COUNT = 3
+METADATA_FILE = "metadata.json"
 
-# Configure logging
-logging.basicConfig(filename='website_monitor.log', level=logging.INFO, 
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+# Logging configuration
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+websites = {}
+
+
+def load_metadata():
+    if os.path.exists(METADATA_FILE):
+        with open(METADATA_FILE, "r") as file:
+            return json.load(file)
+    return {}
+
+
+def save_metadata(metadata):
+    with open(METADATA_FILE, "w") as file:
+        json.dump(metadata, file, indent=4)
+
+
+def update_metadata(website, screenshot_path):
+    metadata = load_metadata()
+    metadata[website] = {
+        "last_captured": time.time(),
+        "screenshot_path": screenshot_path,
+    }
+    save_metadata(metadata)
+
 
 def get_website_ip(website_url):
     try:
         return socket.gethostbyname(website_url)
-    except socket.error:
-        return 'IP Not Found'
+    except (socket.error, socket.gaierror):
+        return "IP Not Found"
 
-# Read websites from the Excel file
-def load_websites_from_excel(file_path):
+
+def capture_screenshot(website_url, output_folder="static/screenshots", delay=3):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    metadata = load_metadata()
+    options = {"headless": True, "args": ["--no-sandbox", "--disable-gpu"]}
+
+    with sync_playwright() as p:
+
+        try:
+            screenshot_filename = (
+                f"{website_url.replace('https://', '').replace('/', '_')}.png"
+            )
+            screenshot_path = os.path.join(output_folder, screenshot_filename)
+
+            if not os.path.exists(screenshot_path):
+                browser = p.chromium.launch(**options)
+                page = browser.new_page()
+                page.goto(website_url)
+                page.screenshot(path=screenshot_path)
+                logging.info(f"Screenshot saved for {website_url}")
+                update_metadata(website_url, screenshot_path)
+                browser.close()
+            else:
+                logging.info(f"Screenshot already exists for {website_url}")
+        except Exception as e:
+            logging.error(f"Error capturing screenshot for {website_url}: {e}")
+        finally:            
+            return screenshot_filename
+
+
+def add_screenshot_to_website(website):
+    global websites
+    try:
+        last_captured = websites[website].get("last_captured")
+        website_ip = websites[website].get("ip")
+        if website_ip == "IP Not Found":
+            return
+        if (
+            last_captured is None or (time.time() - last_captured) > 3600
+        ):  # Check if more than an hour has passed
+            screenshot_path = capture_screenshot(website)
+            websites[website]["screenshot"] = screenshot_path
+    except Exception as e:
+        logging.error(f"Error adding screenshot for {website}: {e}")
+
+
+def load_websites_from_excel(file_path: str = "website_monitor\\domainler.xlsx"):
+    global websites
     try:
         df = pd.read_excel(file_path)
-        df['Domain'] = df['Domain'].str.strip()  # Remove leading/trailing whitespace
-        df['Domain'] = df['Domain'].str.lower()  # Convert to lowercase
-        data = {}
+        df["Domain"] = (
+            df["Domain"].str.strip().str.lower()
+        )  # Remove leading/trailing whitespace and convert to lowercase
+        metadata = load_metadata()
 
-        for url in df['Domain'].dropna():
-            ip_address = get_website_ip(url.strip())
-            data[f"https://www.{url.strip()}/"] = {"ip": ip_address, "status": "Unknown", "error_count": 0, "status_code": 0}
-        return data
+        for url in df["Domain"].dropna():
+            website = f"https://www.{url}/"
+            ip_address = get_website_ip(url)
+            websites[website] = {
+                "ip": ip_address,
+                "status": "Unknown",
+                "error_count": 0,
+                "status_code": 0,
+                "last_captured": metadata.get(website, {}).get("last_captured"),
+            }
+            scr_thread = Thread(
+                target=add_screenshot_to_website, args=(website,))
+            scr_thread.start()
+
+        logging.info("Websites loaded from Excel")
+        return websites
     except Exception as e:
         logging.error(f"Error loading Excel file: {e}")
         return {}
 
-# Load websites from Excel
-websites = load_websites_from_excel('website_monitor\\domainler.xlsx')
 
-# Constants
-ERROR_THRESHOLD = 3  # Alert if down for 3 consecutive checks
-RETRY_COUNT = 2
+# Load websites from Excel
+load_websites_from_excel()
+
 
 def send_email_alert(website):
     try:
-        with smtplib.SMTP_SSL("mail.karehalilar.com", 465) as server:
+        with smtplib.SMTP_SSL(EMAIL_SERVER, EMAIL_PORT) as server:
             server.login(EMAIL_SENDER, EMAIL_PASSWORD)
             subject = f"Website Down Alert: {website}"
-            body = f"ALERT: The website {website} is down for {ERROR_THRESHOLD} consecutive checks."
+            body = f"ALERT: The website {website} is down for {
+                ERROR_THRESHOLD} consecutive checks."
             message = f"Subject: {subject}\n\n{body}"
             server.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, message)
             logging.info(f"Email alert sent for {website}")
@@ -58,22 +153,23 @@ def send_email_alert(website):
         logging.error(f"Failed to send email for {website}. Exception: {e}")
 
 
-    
-            
 def check_website(url):
     global websites
     for _ in range(RETRY_COUNT + 1):
         try:
-            response = requests.get(url, timeout=10)
-            websites[url]["status_code"] = response.status_code 
-            if response.status_code == 200:
-                websites[url]["status"] = "Up"                
-                websites[url]["error_count"] = 0  # Reset error count
-                logging.info(f"Website is available: {url}")
-                return True
-            else:
-                logging.warning(f"Error: {url} returned status code {response.status_code}")
-        except requests.exceptions.RequestException as e:
+            with requests.get(url, timeout=30) as response:
+                websites[url]["status_code"] = response.status_code
+                if response.status_code == 200:
+                    websites[url]["status"] = "Up"
+                    websites[url]["error_count"] = 0  # Reset error count
+                    logging.info(f"Website is available: {url}")
+                    return True
+                else:
+                    logging.warning(
+                        f"Error: {url} returned status code {
+                            response.status_code}"
+                    )
+        except requests.exceptions.ConnectionError as e:
             logging.error(f"Error: {url} is not reachable. Exception: {e}")
 
     websites[url]["error_count"] += 1  # Increment error count
@@ -81,24 +177,24 @@ def check_website(url):
         websites[url]["status"] = "Down"
     else:
         websites[url]["status"] = "Error"
+        # send_email_alert(url)
 
-    # Send email alert if down
-    send_email_alert(url)
-    
-    logging.error(f"{url} has been down for {websites[url]['error_count']} consecutive checks")
+    logging.error(
+        f"{url} has been down for {
+            websites[url]['error_count']} consecutive checks"
+    )
     return False
 
-def check_websites():
-    threads = []
-    for website in websites:
-        thread = threading.Thread(target=check_website, args=(website,))
-        threads.append(thread)
-        thread.start()
-    for thread in threads:
-        thread.join()
 
-# Continuous background check (for the Flask app)
-def start_monitoring():
+def check_websites():
+
+    for website in websites:
+
+        web_thread = Thread(target=check_website, args=(website,))
+        web_thread.start()
+
+
+def periodic_monitoring(interval=3600):  # 1 hour by default
     while True:
         check_websites()
-        time.sleep(1800)  # Check every 5 minutes
+        time.sleep(interval)
