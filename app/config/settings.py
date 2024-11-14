c95c0a52d41c25be63e77ed9532d6654a5faaf34
Great0S <24699619+Great0S@users.noreply.github.com>
2024-11-14 10:46:40 +0300

    Merge branch 'stage' of https://github.com/Great0S/SiteSentinel into stage

--- app/config/settings.py
+++ app/config/settings.py
@@ -9,7 +9,7 @@ class DatabaseSettings(BaseSettings):
     
     db_driver: str = "postgresql+asyncpg"
     db_host: str = Field(..., env="DB_HOST")
-    db_port: int = Field(..., env="DB_PORT")
+    db_port: str = Field(..., env="DB_PORT")
     db_name: str = Field(..., env="DB_NAME")
     db_user: str = Field(..., env="DB_USER")
     db_password: str = Field(..., env="DB_PASSWORD")
@@ -17,13 +17,14 @@ class DatabaseSettings(BaseSettings):
 class Settings(BaseSettings):
     app_name: str = "Site Sentinel App"
     database: DatabaseSettings = DatabaseSettings()
+    database_url: str = f"{database.db_driver}://{database.db_user}:{database.db_password}@{database.db_host}:{database.db_port}/{database.db_name}"
     
     # Initialize the OAuth2 scheme
     oauth2_scheme: OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl="token")
 
     # Email settings
     email_server: str = Field(..., env="EMAIL_SERVER")
-    email_port: int = Field(..., env="EMAIL_PORT")
+    email_port: str = Field(..., env="EMAIL_PORT")
     email_sender: str = Field(..., env="EMAIL_SENDER")
     email_password: str = Field(..., env="EMAIL_PASSWORD")
     email_receiver: str = Field(..., env="EMAIL_RECEIVER")
@@ -34,8 +35,6 @@ class Settings(BaseSettings):
     # Metadata file path
     metadata_file: str = Field(default="app/website_monitor/domainler.xlsx", env="METADATA_FILE")
 
-    # Database settings as a nested class
-    database: DatabaseSettings
 
     class Config:
         env_file = ".env"  # Specify the .env file to load environment variables from
